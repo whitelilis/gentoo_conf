b21d04574c15e9620e9d8485b714d8392bd40513
whitelilis <whitelilis@gmail.com>
2011-11-23 16:39:31 +0800

    for ccl init

---
+++ _ccl-init.lisp
@@ -0,0 +1,8 @@
+
+;;; The following lines added by ql:add-to-init-file:
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+(setf ccl:*default-file-character-encoding* :utf-8) ; for ccl to load utf-8 file
+
