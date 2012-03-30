7a94199c0cf2f047a788e183608159ee630e1647
whitelilis <whitelilis@gmail.com>
2012-03-30 17:42:16 +0800

    add more epla repo

--- emacs-conf/config/package.el
+++ emacs-conf/config/package.el
@@ -1,3 +1,34 @@
 (require 'package)
 (package-initialize)
 
+
+;; add other repo
+  (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                           ("gnu" . "http://elpa.gnu.org/packages/")
+                           ("marmalade" . "http://marmalade-repo.org/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/")
+                           ))
+
+;; make package useable
+
+(defvar prelude-packages
+  '(rainbow-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
+;;; prelude-packages.el ends here
