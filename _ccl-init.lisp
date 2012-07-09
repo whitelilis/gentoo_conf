523203d91c215cc470547514d1c72857a05a5808
whitelilis <whitelilis@gmail.com>
2012-07-09 13:49:58 +0800

    change config to use ccl; adjust encoding config

--- _ccl-init.lisp
+++ _ccl-init.lisp
@@ -5,14 +5,21 @@
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 (setf *print-pretty* t)
+
+
 ;(proclaim '(optimize (debug 3)))
 
 
-;(setf ccl:*default-file-character-encoding* :utf-8) ; for ccl to load utf-8 file
-;(setf ccl:*default-external-format*
-;      (ccl:make-external-format :character-encoding :utf-8
-;                                :line-termination :unix)
-;      ccl:*default-file-character-encoding* :utf-8
-;      ccl:*default-socket-character-encoding* :utf-8
-;      ccl:*terminal-character-encoding-name* :utf-8)
+(setf ccl:*default-file-character-encoding* :utf-8) ; for ccl to load utf-8 file
+(setf ccl:*default-external-format*
+      (ccl:make-external-format :character-encoding :utf-8
+                                :line-termination :unix)
+      ccl:*default-file-character-encoding* :utf-8
+      ccl:*default-socket-character-encoding* :utf-8
+      ccl:*terminal-character-encoding-name* :utf-8)
+
+; for remote slime
+;(load "/home/liuzhe/quicklisp/dists/quicklisp/software/slime-20110730-cvs/swank-loader.lisp")
+;(funcall (read-from-string "swank-loader:init"))
+;(swank:create-server :port 4005 :style :spawn :dont-close t :coding-system "utf-8-unix")
 
