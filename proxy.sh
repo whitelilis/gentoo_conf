fef49893b3d04f76f2f9fdf1f4ee9d1ba2a993c5
whitelilis <whitelilis@gmail.com>
2014-02-19 12:50:02 +0800

    crontab often use different PATH, so change some path in proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+lsof=/usr/sbin/lsof
+sshpass=/usr/local/bin/sshpass
+
 
 function proxy(){
     port=7070
-    lsof -i :$port
+    $lsof -i :$port
     if [ $? -eq 0 ] # proxy started
     then
         exit 0
     else
-        sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 22 || \
-        sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 22 || \
-        sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 25 || \
-        sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 25
+        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 22 || \
+        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 22 || \
+        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 25 || \
+        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 25
     fi
 }
 
