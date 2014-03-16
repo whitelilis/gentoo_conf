faaec0188f2254d983ce9366414ee6fe9a7cf3d8
whitelilis <whitelilis@gmail.com>
2014-03-16 16:25:34 +0800

    add ps ux | grep to proxy.sh

--- tools/proxy.sh
+++ tools/proxy.sh
@@ -15,6 +15,7 @@ function proxy(){
         $sshpass -p btssh.com ssh -fnCTN -D $port btssh@192.228.104.237 -p 25 || \
         $sshpass -p btssh.com ssh -fnCTN -D $port btssh@198.148.115.128 -p 25
     fi
+    ps ux | grep -v grep | grep $port
 }
 
 proxy
