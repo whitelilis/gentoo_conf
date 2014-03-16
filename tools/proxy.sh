bc9fd5d1b463cc1eacb1c12628d74a44e13e4cb2
whitelilis <whitelilis@gmail.com>
2014-03-16 15:19:37 +0800

    update proxy.sh

--- tools/proxy.sh
+++ tools/proxy.sh
@@ -10,10 +10,10 @@ function proxy(){
     then
         exit 0
     else
-        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 22 || \
-        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 22 || \
-        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@192.228.104.237 -p 25 || \
-        $sshpass -p btsshcom ssh -fnCTN -D $port btssh@198.148.115.128 -p 25
+        $sshpass -p btssh.com ssh -fnCTN -D $port btssh@192.228.104.237 -p 22 || \
+        $sshpass -p btssh.com ssh -fnCTN -D $port btssh@198.148.115.128 -p 22 || \
+        $sshpass -p btssh.com ssh -fnCTN -D $port btssh@192.228.104.237 -p 25 || \
+        $sshpass -p btssh.com ssh -fnCTN -D $port btssh@198.148.115.128 -p 25
     fi
 }
 
